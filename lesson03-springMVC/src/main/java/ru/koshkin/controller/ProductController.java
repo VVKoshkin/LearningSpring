00157f42a3846d7da558015e15448856d69350a3
VVKoshkin <humble94@yandex.ru>
2021-04-17 16:44:43 +0300

    Третий урок: Spring MVC: классы второго урока впихнули в браузер

---
+++ lesson03-springMVC/src/main/java/ru/koshkin/controller/ProductController.java
@@ -0,0 +1,53 @@
+package ru.koshkin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.koshkin.Product;
+import ru.koshkin.ProductRepo;
+import ru.koshkin.Storable;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private final ProductRepo repo;
+
+    @Autowired
+    public ProductController(ProductRepo repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public String productListMap(@RequestParam(name = "delete", required = false) Integer id, Model model) {
+        if (id != null) {
+            repo.delete(id);
+        }
+        model.addAttribute("prodList", repo.getAll());
+        return ("product");
+    }
+
+    @GetMapping("/{id}")
+    public String productFormMap(@PathVariable("id") int id, Model model) {
+        model.addAttribute("product", repo.getById(id));
+        return "product_form";
+    }
+
+    @PostMapping
+    public String submitForm(Product product) throws ReflectiveOperationException {
+        repo.put(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/new")
+    public String productNewMap(Model model) throws ReflectiveOperationException {
+        Storable product = new Product(null, null, null);
+        model.addAttribute("product", product);
+        return "product_form";
+    }
+}
